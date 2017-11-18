ec6b243f289905e179665bfb6369677414ec0e5e
Adam Kauth <adamkauth@gmail.com>
2017-11-17 23:37:46 -0800

    Nick did this. Added cam movements.

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour {
+	public Transform playerLocation;
+	
+	private float distanceFromPlayer = 10.0f;
+	public void Update()
+	{
+	   transform.position = playerLocation.position + new Vector3(0,0,-distanceFromPlayer);
+		Debug.Log("Camera: " + transform.position.ToString());
+	}
+}
