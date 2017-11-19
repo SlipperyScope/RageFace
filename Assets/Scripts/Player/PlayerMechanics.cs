c749e18e37b34ef5199d5fd8e5a60bfefe9c8a0c
Michael Lange <dingoeatingfuzz@gmail.com>
2017-11-19 00:20:34 -0800

    Punch implementation

--- Assets/Scripts/Player/PlayerMechanics.cs
+++ Assets/Scripts/Player/PlayerMechanics.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMechanics : MonoBehaviour {
 
+	public float punchForce = 50;
+	public float punchRange = 10;
+
 	private Animator anim;
 
 	// Use this for initialization
@@ -20,10 +23,30 @@ public class PlayerMechanics : MonoBehaviour {
 	}
 
 	void Punch() {
+		var origin = transform.position;
 		var offset = transform.right * 1.13f;
-		Debug.DrawRay(transform.position, transform.up * 10, Color.yellow, 1f);
-		Debug.DrawRay(transform.position + offset, transform.up * 10, Color.yellow, 1f);
-		Debug.DrawRay(transform.position + offset * -1, transform.up * 10, Color.yellow, 1f);
-		Debug.Log(transform.eulerAngles.ToString());
+
+		var hits = new HashSet<RaycastHit2D>();
+		foreach (var hit in Physics2D.RaycastAll(origin, transform.up, punchRange)) {
+			hits.Add(hit);
+		}
+		foreach (var hit in Physics2D.RaycastAll(origin + offset, transform.up, punchRange)) {
+			hits.Add(hit);
+		}
+		foreach (var hit in Physics2D.RaycastAll(origin + -offset, transform.up, punchRange)) {
+			hits.Add(hit);
+		}
+		
+		foreach(var hit in hits) {
+			if (hit.transform == transform) { continue; }
+
+			var rb = hit.transform.gameObject.GetComponent<Rigidbody2D>();
+			if (rb) {
+				rb.AddForce(
+					new Vector2(transform.up.x, transform.up.y) * punchForce,
+					ForceMode2D.Impulse
+				);
+			}
+		}
 	}
 }
