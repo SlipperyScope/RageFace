d7f0ca9f03d7f3b393457c901b824be327b1e97f
Adam Kauth <adamkauth@gmail.com>
2017-11-19 01:29:34 -0800

    Auras give damage to enemies and enemies eventually die

--- Assets/Scripts/parentEnemy.cs
+++ Assets/Scripts/parentEnemy.cs
@@ -6,20 +6,37 @@ public class parentEnemy : MonoBehaviour {
     public int damage;
     public float score;
     public float health;
+
+    private bool isCooling = false;
+    private float coolDownTime = .15f;
     private PlayerStats stats;
 
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
-    }
-
-    public void enemyDead(float hitpointsTakenByBadGuy)
-    {
         if(health <= 0)
         {
-            stats = GetComponent <PlayerStats>();
+            stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
             stats.addToPlayerScore(score);
             gameObject.SetActive(false);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (!isCooling && col.gameObject.CompareTag("Aura"))
+        {
+            if (col.gameObject.GetComponent<SpriteRenderer>().enabled)
+            {
+                isCooling = true;
+                float damage = col.gameObject.GetComponent<auraChecking>().damage;
+                TakeDamage(damage);
+                Invoke("stopCooling", coolDownTime);
+            }
+        }
+    }
+    private void stopCooling()
+    {
+        isCooling = false;
     }
 }
