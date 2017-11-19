f1beaf2af31c63e07eec14a4ea43f0e2f7cd0b6a
NAccuardi <naccuardi@gmail.com>
2017-11-18 17:21:08 -0800

    Health Bar and Player Health implemented.

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerStats : MonoBehaviour {
+	[SerializeField]
+	private Stat health;
+
+
+	// Use this for initialization
+	private void Awake()
+	{
+		health.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			health.CurrentVal-=10;
+		}
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+			health.CurrentVal+=10;
+		}
+	}
+}
